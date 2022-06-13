aeed7499de88c5c93e90646583bf10f5176f1027
StefiMS <tefi_154@hotmail.com>
2022-06-13 17:38:42 -0500

    first steps - react

---
+++ burger-queen/src/Components/login.js
@@ -0,0 +1,22 @@
+
+
+ const login = () =>{
+    
+    return (
+        <>
+          <header>
+             <img id='logoBurguer' alt='logoBurguer'/>
+          </header>
+          <main>
+             <div id='containerLogin'>
+                <label>USUARIO</label>
+                <input type='text'  placeholder='Ingrese su usuario'/>
+                <label>CONTRASEÑA</label>
+                <input type='password' placeholder='Ingrese contraseña' />
+             </div>
+             <button id='buttonLogin'>INICIAR SESIÓN</button>
+          </main>
+        </>
+    )
+ } 
+ 
