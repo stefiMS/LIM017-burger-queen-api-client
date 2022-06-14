c142f813185e6ea8c9c8229c0bb349bb0cf907e6
StefiMS <tefi_154@hotmail.com>
2022-06-13 19:09:05 -0500

    css on login

--- burger-queen/src/Components/login.js
+++ burger-queen/src/Components/login.js
@@ -1,22 +1,26 @@
-
-
+import React from 'react'
+// import './App.css';
+import logoBurguer from './../Assets/logoBurguer.png'
+  
  const login = () =>{
     
     return (
         <>
           <header>
-             <img id='logoBurguer' alt='logoBurguer'/>
+             <img src={logoBurguer} id='logoBurguer' alt='logoBurguer' />
           </header>
           <main>
              <div id='containerLogin'>
                 <label>USUARIO</label>
-                <input type='text'  placeholder='Ingrese su usuario'/>
+                <input type='text'  placeholder='Ingrese su usuario' className='inputLogin'/>
                 <label>CONTRASEÑA</label>
-                <input type='password' placeholder='Ingrese contraseña' />
+                <input type='password' placeholder='Ingrese contraseña' className='inputLogin'/>
              </div>
-             <button id='buttonLogin'>INICIAR SESIÓN</button>
+             {/* <button id='buttonLogin'>INICIAR SESIÓN</button> */}
+             <button type="button" id="buttonLogin" class="btn btn-warning">INICIAR SESIÓN</button>
           </main>
         </>
     )
  } 
  
+ export default  login;
