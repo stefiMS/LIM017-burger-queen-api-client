8144a4609f49ea17729763a6223feceb64b1bdd0
StefiMS <tefi_154@hotmail.com>
2022-06-26 18:11:07 -0500

    working with port 3031

--- burger-queen/src/util/Authentication.js
+++ burger-queen/src/util/Authentication.js
@@ -1,45 +1,45 @@
 import axios from "axios";
  
-const urlAxios = "http://localhost:8080/login";
+// const urlAxios = "http://localhost:8081/login";
  
-export const authentication = (email, password) => {
-  return new Promise((resolve, reject) => {
-    axios
-      .post(urlAxios, { email, password })
-      .then((response) => {
-        const { status, data } = response;
-        if (status === 200) {
-          resolve(data);
-        }
-        reject(status);
-      })
-      .catch((error) => {
-        reject(error);
-      }); 
-  });
-};
-
-
-
-//   export const authentication = (email, password) => {
-
+// export const authentication = (email, password) => {
 //   return new Promise((resolve, reject) => {
-//     axios.post(urlAxios, { email, password })
-//     .then((response) => {   
-//       const  { status  }  = response;  
-//       if (status === 200) {
-//         const  { token }  = response.data;
-//         resolve(token);
-//       }else{
-//         reject(response.data);
-       
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(error)
-//       reject(error);
-//       // console.log(error)
-//     })
+//     axios
+//       .post(urlAxios, { email, password })
+//       .then((response) => {
+//         const { status, data } = response;
+//         if (status === 200) {
+//           resolve(data);
+//         }
+//         reject(status);
+//       })
+//       .catch((error) => {
+//         reject(error);
+//       }); 
 //   });
-// }
+// };
+
+const urlAxios = "http://localhost:3001/auth";
+
+  export const authentication = (email, password) => {
+
+  return new Promise((resolve, reject) => {
+    axios.post(urlAxios, { email, password })
+    .then((response) => {   
+      const  { status  }  = response;  
+      if (status === 200) {
+        const  { token }  = response.data;
+        resolve(token);
+      }else{
+        reject(response.data);
+       
+      }
+    })
+    .catch((error) => {
+      // console.log(error)
+      reject(error);
+      // console.log(error)
+    })
+  });
+}
 
